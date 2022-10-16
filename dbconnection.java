f9017efd72cd552963612f8d3779e6b16bec3b8d
venkatpallepogu3 <115933468+venkatpallepogu3@users.noreply.github.com>
2022-10-16 20:41:05 +0530

    Add files via upload

---
+++ dbconnection.java
@@ -0,0 +1,31 @@
+	import java.sql.*;
+	import java.io.*;
+
+     class dbconnection
+      {
+	public static void main(String args[])
+		{  
+		try
+		{  
+		//step1 load the driver class  
+		Class.forName("oracle.jdbc.driver.OracleDriver");  
+		  
+		//step2 create  the connection object  
+		Connection con=DriverManager.getConnection(  
+		"jdbc:oracle:thin:@localhost:1522:xe","system","oracle");
+		Statement st= con.createStatement();
+		ResultSet rs=st.executeQuery("select *from venkat");
+		while(rs.next())
+		{
+			System.out.println(rs.getString(1)+"  "+rs.getString(2));
+		}
+		System.out.println("connection ok");
+        }
+		catch(Exception e)
+		{
+			System.out.println(e);
+			System.out.println("not connected");
+		}
+
+
+}}
